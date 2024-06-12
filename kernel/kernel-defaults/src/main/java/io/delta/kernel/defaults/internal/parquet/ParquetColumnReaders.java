6a879b51b498ffcf5cf95f41292e4228c32336aa
Richard Chen <r.chen@databricks.com>
2024-06-11 17:03:14 -0700

    remove write changes and add toKernelType to read variants with differnt physical storage representations

--- kernel/kernel-defaults/src/main/java/io/delta/kernel/defaults/internal/parquet/ParquetColumnReaders.java
+++ kernel/kernel-defaults/src/main/java/io/delta/kernel/defaults/internal/parquet/ParquetColumnReaders.java
@@ -83,11 +83,13 @@ class ParquetColumnReaders {
         } else if (typeFromClient instanceof TimestampNTZType) {
             return createTimestampNtzConverter(initialBatchSize, typeFromFile);
         } else if (typeFromClient instanceof VariantType) {
+            // TODO(r.chen): Is converting the typeFromFile to the readSchea ok?
+            // We lose the field metadata from the client.
             return new RowColumnReader(
                 initialBatchSize,
-                new StructType()
-                    .add("value", BinaryType.BINARY, false)
-                    .add("metadata", BinaryType.BINARY, false),
+                // The physical schema representing variants can be different per file so we must
+                // infer the read schema from the type from file.
+                (StructType) ParquetSchemaUtils.toKernelType(typeFromFile),
                 (GroupType) typeFromFile);
         }
 
